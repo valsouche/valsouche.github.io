e89979c55ecda7752e81694dd7ce485592cb3643
Valentin Souche <valentin.souche@teads.tv>
2016-09-30 16:50:49 +0200

    Dev in progress Typed.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,6 @@
-/**
- * Created by vsouche on 30/09/2016.
- */
-$(document).ready(function() {
-var movementStrength = 25;
-var height = movementStrength / $(window).height();
-var width = movementStrength / $(window).width();
-$("#top-image").mousemove(function(e){
-          var pageX = e.pageX - ($(window).width() / 2);
-          var pageY = e.pageY - ($(window).height() / 2);
-          var newvalueX = width * pageX * -1 - 25;
-          var newvalueY = height * pageY * -1 - 50;
-          $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
-});
-});
+$(function(){
+        $(".skills").typed({
+            strings: ["Javascript", "AngularJS <3", "Grunt", "NPM", "NodeJS", "Git", "Yeoman", "& all others Front family members :)" ],
+            typeSpeed: 0
+        });
+    });
