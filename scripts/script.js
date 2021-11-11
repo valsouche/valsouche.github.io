13d7ed33f32c56e4983a4b3475f33e7fddf2d658
Valentin Souche <valentin.souche@gmail.com>
2021-11-11 21:44:18 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 $(function(){
         $(".skills").typed({
-            strings: ["Teads", "Dev", "Scala", "Typescript", "Node & React", "AWS", "Sport" ],
+            strings: ["Teads", "Dev", "Scala", "Typescript", "NodeJS", "React", "AWS", "Sport" ],
             typeSpeed: 50,
             shuffle: false,
             loop: true
